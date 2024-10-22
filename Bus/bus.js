ea9431f69d9c28bf728c2b87155c046aa9ad7bd3
AdityaPrasadSwain <swainaditya921@gmail.com>
2024-10-22 07:02:18 +0000

    comment delete

--- Bus/bus.js
+++ Bus/bus.js
@@ -2,10 +2,8 @@ function swapValues() {
     var input1 = document.getElementById('input1'); 
     var input2 = document.getElementById('input2'); 
      
-    // Temporarily hold the value of the first input 
     var temp = input1.value; 
      
-    // Swap the values 
     input1.value = input2.value; 
     input2.value = temp; 
     }
